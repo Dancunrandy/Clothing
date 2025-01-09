0c02bf43e8f95128c43d6dfea6972d7b89e1b658
dancunrandy <dancunrandy@gmail.com>
2025-01-09 11:20:39 +0300

    routing

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+
+function App() {
+  return (
+    <div >
+      <Router>
+      <Switch>
+        <Route exact path='/' component={ Homepage}/>
+        <Route  path='/Shop' component={ ShopPage}/>
+      </Switch>
+        
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
