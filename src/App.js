4e68d7ab7b7316216af925bda2d83a33a803e661
dancunrandy <dancunrandy@gmail.com>
2025-01-11 11:50:48 +0300

    Switch is replaced by Routes: React Router v6 uses Routes to wrap Route components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-
+import SignInAndSignUpPage from './pages/SignInAndSignUp/SignInAndSignUp.component';
 
 function App() {
   return (
-    <div >
+    <div>
       <Router>
-      <Switch>
-      <Header/>
-        <Route exact path='/' component={ Homepage}/>
-        <Route  path='/Shop' component={ ShopPage}/>
-      </Switch>
-        
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Shop" element={<ShopPage />} />
+          <Route path="/Signin" element={<SignInAndSignUpPage />} />
+        </Routes>
       </Router>
-
     </div>
   );
 }
