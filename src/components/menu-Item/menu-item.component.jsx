f5a8e95bf1d51969ff9ba4711a0289ff1415632a
dancunrandy <dancunrandy@gmail.com>
2025-01-11 11:48:28 +0300

    n React Router v6, withRouter is no longer available, so we replaced it with the useNavigate hook

--- src/components/menu-Item/menu-item.component.jsx
+++ src/components/menu-Item/menu-item.component.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-import {withRouter} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size,history,linkUrl}) => (
-    <div className={`${size} menu-item`} onClick={()=> history.push(`${matchMedia.url}${linkUrl}`)} >
-    <div className='background-image'  style={{
-        backgroundImage:`url(${imageUrl})`
-    }} />
-    <div className="content">
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate(); // Use the useNavigate hook to access the navigation function
+
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => navigate(linkUrl)} // Use navigate to change routes
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-</div>
-)
+  );
+};
 
-export default withRouter(MenuItem);
+export default MenuItem;
