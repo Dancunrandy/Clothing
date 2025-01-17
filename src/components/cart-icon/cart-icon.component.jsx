0aa5b1be2196525e2624359a26ed6c97cde40d11
dancunrandy <dancunrandy@gmail.com>
2025-01-17 09:44:28 +0300

    updated application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,22 +2,27 @@ import React from "react";
 
 import { connect } from "react-redux";
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import {ReactComponent as ShoppingIcon} from './../../assets/11.2 shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
 const mapDispatchToProps = dispatch =>({
     toggleCartHidden:() => dispatch(toggleCartHidden())
 })
+//  reduce() function accumulates quantity on cart icon, in other words a Selector.
+const mapStateToProps = state => ({          //updated code from cart selectors js file
+    itemCount:selectCartItemsCount(state)
+})
 
-
-export default connect( null,mapDispatchToProps) (CartIcon);
+export default connect( mapStateToProps,mapDispatchToProps) (CartIcon);
--- src/components/cart/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from '../custom-button/button.component';
 import './cart-dropdown.styles.scss';
@@ -16,8 +17,10 @@ const CartDropdown = ({cartItems}) => (
 
 );
 
-const mapStateToProps = ({cart:{cartItems}}) => ({
-    cartItems
+// use  of reselect from cart selectors will allow us to memoize and not re-render a component if the state value does not change
+
+const mapStateToProps = state => ({   
+    cartItems:selectCartItems(state)
 })
 
 export default connect(mapStateToProps) (CartDropdown);
