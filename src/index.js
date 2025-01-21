66648392bb382eb8f351528aa3e3aa9d82538586
dancunrandy <dancunrandy@gmail.com>
2025-01-21 11:36:21 +0300

    moved directory state into redux

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Provider store={store}>
-      <React.StrictMode>
-         <App />
-      </React.StrictMode>
+  <React.StrictMode>
+       <PersistGate persistor={persistor}>
+             <App />
+    </PersistGate>
+</React.StrictMode>
 
 </Provider>
 
