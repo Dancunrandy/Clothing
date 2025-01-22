345dd80defd3765e2329e7eb7cdc4b16494b729c
dancunrandy <dancunrandy@gmail.com>
2025-01-22 12:49:00 +0300

    added stripe as payment method

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import {connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 
 import './checkout.styles.scss';
 
@@ -33,7 +35,13 @@ const CheckoutPage = ({cartItems, total}) => (
             }
             <div className="total">
                 <span>TOTAL:${total}</span>
-            </div>
+         </div>
+         <div className="test-warning">
+            *Kindly test payment with this credit card *
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/26 - cvv: 123
+         </div>
+            <StripeCheckoutButton price={total}/>
     </div>
 )
 
